9dad28953a0d53990a55271685c55017bc8df6ed
unknown <amberj.kolar@gmail.com>
2020-06-09 09:43:20 -0700

    test Board exists

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,11 @@ class Game
     @board = board
   end
   def over?
+    # return (@board[:top][:left] == nil || @board[:top][:middle] == nil ||
+    #   @board[:top][:right] == nil || @board[:middle][:left] == nil ||
+    #   @board[:middle][:middle] == nil || @board[:middle][:right] == nil ||
+    #   @board[:bottom][:left] == nil || @board[:bottom][:middle] == nil ||
+    #   @board[:bottom][:right] == nil)
     false
   end
 end
